e9da630f88a4dee830741d81a0a6fb9d9314dc3e
Erik Waterham <waterham@gmail.com>
2020-01-06 12:52:08 +0100

    add JIP4iD, empty project

---
+++ JIP4iD/JIP4iD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JIP4iD
+//
+//  Created by Erik Waterham on 06/01/2020.
+//  Copyright © 2020 Erik Waterham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
