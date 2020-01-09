31e7e314d25aca23b4f18e3ca3a5714b972b814a
Erik Waterham <waterham@gmail.com>
2020-01-09 10:12:41 +0100

    refactor Content View, Network Manager Movie and Network Manager Movie Popular

---
+++ JIP4iD/JIP4iD/ImageLoader.swift
@@ -0,0 +1,9 @@
+//
+//  ImageLoader.swift
+//  JIP4iD
+//
+//  Created by Erik Waterham on 09/01/2020.
+//  Copyright © 2020 Erik Waterham. All rights reserved.
+//
+
+import Foundation
---
+++ JIP4iD/JIP4iD/ImageView.swift
@@ -0,0 +1,9 @@
+//
+//  ImageView.swift
+//  JIP4iD
+//
+//  Created by Erik Waterham on 09/01/2020.
+//  Copyright © 2020 Erik Waterham. All rights reserved.
+//
+
+import Foundation
