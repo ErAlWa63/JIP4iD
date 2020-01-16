d079b8c8bf37c48447d800c7f67d3bb602d0749c
Erik Waterham <waterham@gmail.com>
2020-01-16 09:37:14 +0100

    refactor swiftlint warning "Explicit ACL Violation: All declarations should specify Access Control Level keywords explicitly. (explicit_acl)" part 4

--- JIP4iD/JIP4iD/Movie.swift
+++ JIP4iD/JIP4iD/Movie.swift
@@ -6,82 +6,82 @@
 //  Copyright © 2020 Erik Waterham. All rights reserved.
 //
 
-struct Movie: Codable {
+public struct Movie: Codable {
 
-    struct BelongsToCollection: Codable, Identifiable {
-        let id: Int
-        let name: String
-        let posterPath: String?
-        let backdropPath: String?
+    public struct BelongsToCollection: Codable, Identifiable {
+        public let id: Int
+        public let name: String
+        public let posterPath: String?
+        public let backdropPath: String?
     }
 
-    struct Genres: Codable, Identifiable {
+    public struct Genres: Codable, Identifiable {
 
-        let id: Int
-        let name: String
+        public let id: Int
+        public let name: String
     }
 
-    struct ProductionCompanies: Codable, Identifiable {
+    public struct ProductionCompanies: Codable, Identifiable {
 
-        let id: Int
-        let logoPath: String?
-        let name: String
-        let originCountry: String
+        public let id: Int
+        public let logoPath: String?
+        public let name: String
+        public let originCountry: String
     }
 
-    struct ProductionCountries: Codable {
+    public struct ProductionCountries: Codable {
 
-        let iso31661: String
-        let name: String
+        public let iso31661: String
+        public let name: String
     }
 
-    struct SpokenLanguages: Codable {
+    public struct SpokenLanguages: Codable {
 
-        let iso6391: String?
-        let name: String?
+        public let iso6391: String?
+        public let name: String?
     }
 
-    struct Videos: Codable {
+    public struct Videos: Codable {
 
-        struct Results: Codable {
+        public struct Results: Codable {
 
-            let id: String
-            let iso6391: String
-            let iso31661: String
-            let key: String
-            let name: String
-            let site: String
-            let size: Int
-            let type: String
+            public let id: String
+            public let iso6391: String
+            public let iso31661: String
+            public let key: String
+            public let name: String
+            public let site: String
+            public let size: Int
+            public let type: String
         }
 
-        let results: [Results]
+        public let results: [Results]
     }
     
-    let adult: Bool
-    let backdropPath: String
-    let belongsToCollection: BelongsToCollection?
-    let budget: Int
-    let genres: [Genres]
-    let homepage: String?
-    let id: Int?
-    let imdbId: String?
-    let originalLanguage: String?
-    let originalTitle: String?
-    let overview: String
-    let popularity: Double?
-    let posterPath: String?
-    let productionCompanies: [ProductionCompanies]?
-    let productionCountries: [ProductionCountries]?
-    let releaseDate: String
-    let revenue: Int?
-    let runtime: Int?
-    let spokenLanguages: [SpokenLanguages]?
-    let status: String?
-    let tagline: String?
-    let title: String
-    let video: Bool
-    let voteAverage: Double?
-    let voteCount: Int?
-    let videos: Videos
+    public let adult: Bool
+    public let backdropPath: String
+    public let belongsToCollection: BelongsToCollection?
+    public let budget: Int
+    public let genres: [Genres]
+    public let homepage: String?
+    public let id: Int?
+    public let imdbId: String?
+    public let originalLanguage: String?
+    public let originalTitle: String?
+    public let overview: String
+    public let popularity: Double?
+    public let posterPath: String?
+    public let productionCompanies: [ProductionCompanies]?
+    public let productionCountries: [ProductionCountries]?
+    public let releaseDate: String
+    public let revenue: Int?
+    public let runtime: Int?
+    public let spokenLanguages: [SpokenLanguages]?
+    public let status: String?
+    public let tagline: String?
+    public let title: String
+    public let video: Bool
+    public let voteAverage: Double?
+    public let voteCount: Int?
+    public let videos: Videos
 }
