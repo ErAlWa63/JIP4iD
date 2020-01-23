133cebb2554e977f879a937831d194efa256357c
Erik Waterham <waterham@gmail.com>
2020-01-23 13:18:57 +0100

    refactor SwiftLint warning "File Header Violation: Header comments should be consistent with project patterns. (file_header)" part 2

--- JIP4iD/JIP4iD/Common/ImageView.swift
+++ JIP4iD/JIP4iD/Common/ImageView.swift
@@ -1,11 +1,3 @@
-//
-//  ImageView.swift
-//  JIP4iD
-//
-//  Created by Erik Waterham on 09/01/2020.
-//  Copyright © 2020 Erik Waterham. All rights reserved.
-//
-
 import SwiftUI
 
 public struct ImageView: View {
