941861b29e2d4f044c7ee439a78edec0dea11a52
Erik Waterham <waterham@gmail.com>
2020-01-23 14:39:02 +0100

    refactor SwiftLint warning "File Header Violation: Header comments should be consistent with project patterns. (file_header)" part 13

--- JIP4iD/JIP4iD/MovieCatalog/SecondScreenMovieCatalog.swift
+++ JIP4iD/JIP4iD/MovieCatalog/SecondScreenMovieCatalog.swift
@@ -1,11 +1,3 @@
-//
-//  SecondScreenMovieCatalog.swift
-//  JIP4iD
-//
-//  Created by Erik Waterham on 22/01/2020.
-//  Copyright © 2020 Erik Waterham. All rights reserved.
-//
-
 import SwiftUI
 
 public struct SecondScreenMovieCatalog: View {
