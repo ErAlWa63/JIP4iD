1b1c282218d9bd94b0218550677da69f9573add8
Erik Waterham <waterham@gmail.com>
2020-01-23 14:41:24 +0100

    refactor SwiftLint warning "File Header Violation: Header comments should be consistent with project patterns. (file_header)" part 16

--- JIP4iD/JIP4iD/NetworkManagerMovie.swift
+++ JIP4iD/JIP4iD/NetworkManagerMovie.swift
@@ -1,11 +1,3 @@
-//
-//  NetworkManagerMovie.swift
-//  JIP4iD
-//
-//  Created by Erik Waterham on 07/01/2020.
-//  Copyright © 2020 Erik Waterham. All rights reserved.
-//
-
 import Combine
 import Foundation
 
