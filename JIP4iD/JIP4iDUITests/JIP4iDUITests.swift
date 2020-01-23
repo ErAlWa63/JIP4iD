043cc18f90893abef1ba81038097c7dcd18d1c86
Erik Waterham <waterham@gmail.com>
2020-01-23 14:51:27 +0100

    refactor SwiftLint warning "File Header Violation: Header comments should be consistent with project patterns. (file_header)" part 23

--- JIP4iD/JIP4iDUITests/JIP4iDUITests.swift
+++ JIP4iD/JIP4iDUITests/JIP4iDUITests.swift
@@ -1,11 +1,3 @@
-//
-//  JIP4iDUITests.swift
-//  JIP4iDUITests
-//
-//  Created by Erik Waterham on 06/01/2020.
-//  Copyright © 2020 Erik Waterham. All rights reserved.
-//
-
 import XCTest
 
 private class JIP4iDUITests: XCTestCase { // swiftlint:disable:this private_unit_test
